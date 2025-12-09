d43e75df3a4b591caba2e91dc8916e5d5a436244
Rémi Moreau <113788943+Scarler04@users.noreply.github.com>
2025-12-09 09:12:42 +0100

    Commenting code

--- Battleship_v2/database.py
+++ Battleship_v2/database.py
@@ -1,36 +1,80 @@
 import os
 import csv
 
-def find_file(filename, search_path="."):
-    for root, dirs, files in os.walk(search_path):
-        if filename in files:
+
+def find_file(filename : str, search_path = ".") :
+    """
+    Search for a file in a directory tree.
+
+    This function walks through all subdirectories starting from
+    `search_path` and returns the full path to `filename` as soon
+    as it is found.
+
+    Parameters
+    ----------
+        filename : str
+            Name of the file to search for
+        search_path : str, optional
+            Directory path from which the search begins (default is current directory)
+
+    Returns
+    -------
+        str or None
+            Full path to the file if found, otherwise None
+    """
+    for root, dirs, files in os.walk(search_path) :
+        if filename in files :
             return os.path.join(root, filename)
     return None
 
-def find_directory(dirname, search_path="."):
-    for root, dirs, files in os.walk(search_path):
-        if dirname in dirs:
+
+def find_directory (dirname : str, search_path = ".") :
+    """
+    Search for a folder in a directory tree
+    
+    Parameters
+    -------
+        dirname : str 
+            Name of the folder to search for
+        search_path : str, optional
+            Directory path from which the search begins (default is current directory) 
+        
+    Returns
+    -------
+        str or None : Full path to the folder if found, otherwise None
+    """
+    for root, dirs, files in os.walk(search_path) :
+        if dirname in dirs :
             return os.path.join(root, dirname)
     return None
 
-def create_database () :
-    path = find_file("scoreboard_battleship.csv")
 
-    if path:
-        return path
-        
-    else:
-        path = find_directory("Battleship_v2")
+def create_database () :
+    """
+    Find or create scoreboard data file
+
+    Returns
+    -------
+        str : Full path to the file if found or created file, otherwise None.
+    """
+    path = find_file(filename= "scoreboard_battleship.csv")
+
+    if path :
+        return path    
+    else :
+        path = find_directory(dirname= "Battleship_v2")
+
+        # Adding default scoreboard
         if path :
             path += "/scoreboard_battleship.csv"
             default_scores = [
                 ["Username", "Score"],
-                ["vanRossum", 25],
-                ["Scarler", 35],
-                ["36", 50]
+                ["vanRossum", 60],
+                ["Scarler", 50],
+                ["36", 70]
             ]
 
-            with open(path, "w", newline="") as f:
+            with open(path, "w", newline= "") as f :
                 writer = csv.writer(f)
                 writer.writerows(default_scores)
 
@@ -38,12 +82,25 @@ def create_database () :
         else :
             return path
 
+
 def update_scoreboard (username : str, score : int, path : str) :
-    with open(path,"a",newline="\n") as f:
+    """
+    Update scoreboard data file after winning a game
+
+    Parameters
+    -------
+        username : str
+            Player username
+        score : int
+            Player score
+        path : str
+            Path to the scoreboard data file
+    """
+    with open(path, "a", newline= "\n") as f :
         writer = csv.writer(f)
         writer.writerow([username,score])
-    pass 
+
 
 if __name__ == "__main__" :
     path = create_database()
-    print(path)
+    update_scoreboard("cici",50,path)
